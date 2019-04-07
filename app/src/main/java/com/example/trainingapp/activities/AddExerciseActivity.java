afaec00a25cf9aa571f71394e946e5134839ab43
Oliver van Komen <olvan18@student.sdu.dk>
2019-04-07 15:26:47 +0200

    Yes

--- app/src/main/java/com/example/trainingapp/activities/AddExerciseActivity.java
+++ app/src/main/java/com/example/trainingapp/activities/AddExerciseActivity.java
@@ -1,4 +1,39 @@
 package com.example.trainingapp.activities;
 
-public class AddExerciseActivity {
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.trainingapp.R;
+
+public class AddExerciseActivity extends AppCompatActivity {
+    private static final String TAG = "AddExerciseActivity";
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_exercise_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected: What");
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_exercise);
+
+
+        Toolbar toolbar = findViewById(R.id.addExerciseToolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
 }
--- app/src/main/java/com/example/trainingapp/activities/WorkoutActivity.java
+++ app/src/main/java/com/example/trainingapp/activities/WorkoutActivity.java
@@ -1,29 +1,69 @@
 package com.example.trainingapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.example.trainingapp.R;
-import com.example.trainingapp.TrainingRoutine;
+import com.example.trainingapp.Workout;
 import com.example.trainingapp.adapters.ExerciseAdapter;
 
-public class RoutineDetailsActivity extends AppCompatActivity implements ExerciseAdapter.OnNoteListener {
-
-    private TrainingRoutine routine = null;
+public class WorkoutActivity extends AppCompatActivity implements ExerciseAdapter.OnNoteListener {
+    private static final String TAG = "WorkoutActivity";
+    private Workout workout = null;
     private RecyclerView exerciseRV;
+    private ExerciseAdapter adapter;
     private Bundle extras;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.workout_overview_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected: What");
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_routine_view);
+        setContentView(R.layout.activity_workout_view);
+
+        //TODO MenuInflater with "Change workout name, set general timer etc..
 
         setUp();
-        ExerciseAdapter adapter = new ExerciseAdapter(routine.getExercises(), this);
+        adapter = new ExerciseAdapter(workout.getExercises(), this);
         exerciseRV.setAdapter(adapter);
+    }
+
+    public void addExercise(View v) {
+        Intent addExerciseIntent = new Intent(this, AddExerciseActivity.class);
+        startActivity(addExerciseIntent);
+
+
+        /*workout.addExercise(new Exercise("Deadlift", Exercise.Category.DUMBBELL));
+        adapter.notifyDataSetChanged();*/
+    }
+
+    @Override
+    public void onNoteClick(int position) {
+        //TODO Exercise History activity
+    }
+
+    @Override
+    public void onLongNoteClick(int position) {
+        //TODO Delete exercise dialog box
     }
 
     private void setUp() {
@@ -35,19 +75,13 @@ public class RoutineDetailsActivity extends AppCompatActivity implements Exercis
         extras = getIntent().getExtras();
 
         if (extras != null) {
-            routine = (TrainingRoutine) extras.getSerializable(MainActivity.TAPPED_ROUTINE_KEY);
+            workout = (com.example.trainingapp.Workout) extras.getSerializable(MainActivity.TAPPED_ROUTINE_KEY);
         }
 
         Toolbar toolbar = findViewById(R.id.routineDetailsToolbar);
-        toolbar.setTitle(routine.getRoutineTitle());
-    }
-
-    @Override
-    public void onNoteClick(int position) {
-    }
-
-    @Override
-    public void onLongNoteClick(int position) {
-
+        toolbar.setTitle(workout.getRoutineTitle());
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
